a92a4d4286cf088e58d17bcdcad688d7fb0461de
Mason Keeney <mason.keeney68@gmail.com>
2022-02-18 19:19:22 -0700

    O3L1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,15 @@
+public class Objective3Lab1 {
+  public static void main(String[] args){
+    //declaring variables... again
+    double num1, num2, num3, average;
+
+    //initializing variables
+    num1 = 3223;
+    num2 = 4434;
+    num3 = 658;
+    average = (num1 + num2 + num3) / 3;
+
+    //output - is this called the method? or is that this whole section?
+    System.out.println(average);
+  }
+}
