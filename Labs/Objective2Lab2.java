943dc09299bc23d04d23b9fe1d3f2b48cdb0cd93
Mason Keeney <mason.keeney68@gmail.com>
2022-02-18 18:59:33 -0700

    O2L2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args){
+
+    //info
+    String name = "Mason Keeney";
+    int age = 32;
+    String state = "Colorado";
+
+    //output
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
