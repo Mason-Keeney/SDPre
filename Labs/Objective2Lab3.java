5cdcd5a32ae567a0e3657ff13c793918e9b2c9e1
Mason Keeney <mason.keeney68@gmail.com>
2022-02-18 19:06:28 -0700

    O2L3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,15 @@
+public class Objective2Lab3 {
+  public static void main(String[] args){
+    //declaring variables
+    double num1, num2, num3, average;
+
+    //initializing variables
+    num1 = 376;
+    num2 = 22.53;
+    num3 = 36.47;
+    average = (num1 + num2 + num3) / 3;
+
+    //output
+    System.out.println(average);
+  }
+}
