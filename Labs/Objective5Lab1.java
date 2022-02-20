a155346b96612ab63d352235e3a71b1e9932dbca
Mason Keeney <mason.keeney68@gmail.com>
2022-02-20 10:25:43 -0700

    O5L1 Finish

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,17 @@
+public class Objective5Lab1{
+  public static void main(String[] args){
+    //declare and initialize variables
+    int num1 = 5;
+    int num2 = 15;
+
+    //conditional statements compare values then run based on comparison
+
+    if (num1 > num2) {
+      System.out.println(num1 + " is greater than " + num2);
+    }
+
+    else if (num2 > num1) {
+      System.out.println(num2 + " is greater than " + num1);
+      }
+    }
+  }
