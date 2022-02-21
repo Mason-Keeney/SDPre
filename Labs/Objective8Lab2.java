2c9847d67e2cced37233890ea2c0fc81720be4f1
Mason Keeney <mason.keeney68@gmail.com>
2022-02-20 19:44:02 -0700

    O8L2 Finish

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+  public static void main(String[] args){
+    int control;
+    int sum = 0;
+
+    for(control = 1; control <=20; control++){
+      sum = sum + control;
+    }
+    System.out.println(sum);
+  }
+}
